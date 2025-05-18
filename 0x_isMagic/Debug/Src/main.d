e85719674587d05e6624bbf971a5c406f044ad0e
Dayana Madrid <dmadrid@unal.edu.co>
2025-05-17 21:11:21 -0500

    archivos random

---
+++ 0x_isMagic/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/core_cm4.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/core_cm4.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_version.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/namontoy/STM32CubeIDE/T5-2024-01/CMSIS-repo/STM32Cube_FW_F4_V1.27.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
